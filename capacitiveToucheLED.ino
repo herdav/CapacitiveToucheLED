89a62dcbed8a70104954b1dd30bad3bd2838cd13
David Herren <herdav.mobile@gmail.com>
2023-10-04 09:58:28 +0200

    Update capacitiveToucheLED.ino

--- capacitiveToucheLED.ino
+++ capacitiveToucheLED.ino
@@ -13,16 +13,22 @@
 CapacitiveSensor sensor = CapacitiveSensor(4, 2); // 1M resistor between pins 4 & 2, pin 2 is sensor pin
 
 bool hldLED = false;
+bool hldDLY = false;
 int sensitivityMAX = 200;
 int sensitivityMIN = 0;
 int setLEDtime = 100;
 int hldLEDtime = 0;
+int setDLYtime = 50;
+int hldDLYtime = 0;
+String isRun = "OFF";
 
 void setup() {
   Serial.begin(115200);
   pinMode(pinLED, OUTPUT);
   pinMode(pinTAS, INPUT);
   pinMode(pinREG, INPUT);
+
+  sensor.set_CS_AutocaL_Millis(0xFFFFFFFF); // turn off autocalibration
 }
 
 void loop() {
@@ -30,8 +36,11 @@ void loop() {
   int stateREG = analogRead(pinREG);
   int valREG = map(stateREG, 0, 1023, sensitivityMIN, sensitivityMAX);
 
+  Serial.print(isRun);
   Serial.print(valREG);
   Serial.print("<");
+  Serial.print(hldDLYtime);
+  Serial.print("_");
   Serial.print(hldLEDtime);
   Serial.print(">");
 
@@ -39,14 +48,23 @@ void loop() {
   Serial.println(measurement);
 
   if (stateTAS || measurement > valREG || hldLED) {
-    digitalWrite(pinLED, HIGH);
-    hldLED = true;
+    hldDLYtime++;
+    if (hldDLYtime >= setDLYtime || hldDLY) {
+      hldDLY = true;
+      digitalWrite(pinLED, HIGH);
+      hldLED = true;
+      isRun = "ON--";
+    }
   } else {
     digitalWrite(pinLED, LOW);
     hldLED = false;
+    isRun = "OFF-";
+    hldDLY = false;
+    hldDLYtime = 0;
   }
   if (hldLED) {
     hldLEDtime++;
+    hldDLYtime = 0;
     if (hldLEDtime >= setLEDtime) {
       hldLED = false;
       hldLEDtime = 0;
