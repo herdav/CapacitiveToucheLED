fe2c2945ca3f6eaeaca4e92bee04fef5f1f086b2
David Herren <herdav.mobile@gmail.com>
2023-10-06 08:16:48 +0200

    Update capacitiveToucheLED.ino

--- capacitiveToucheLED.ino
+++ capacitiveToucheLED.ino
@@ -1,26 +1,34 @@
-// CAPACITIVE TOUCH LED
+// capacitiveToucheLED
+// Version 231006
 // Created 2023 by David Herren
 // https://davidherren.ch
 // https://github.com/herdav/capacitiveTouchLED
-// --------------------------------------
+// --------------------------------------------
 
 #include <CapacitiveSensor.h>
 
-#define PIN_TAS 10 // Taster pin
-#define PIN_LED 8  // LED pin
-#define PIN_REG A0 // Sensitivity control pin (40/200)
+#define PIN_TAS 10 // Taster pin.
+#define PIN_LED 8  // LED pin.
+#define PIN_REG A0 // Sensitivity control pin (50/100).
 
-CapacitiveSensor sensor = CapacitiveSensor(4, 2); // 1M resistor between pins 4 & 2, pin 2 is sensor pin
+CapacitiveSensor sensor = CapacitiveSensor(4, 2); // 1M resistor between pins 4 & 2, pin 2 is sensor pin.
 const int AUTO_CALIBRATION_OFF = 0xFFFFFFFF;
 
 bool ledActive = false;
 bool delayActive = false;
-int sensitivityMAX = 200;
-int setLEDtime = 100;
-int ledOnTime = 0;
-int setDelayTime = 50;
-int delayTime = 0;
+int setSensitivityMax = 100;
+int setLEDtime = 20; // 100
+int countLedOnTime = 0;
+int countLedActiveTime = 0;
+int setDelayTime = 50; // 50
+int countDelayTime = 0;
+int countWaitingTime = 0; // Counts time while sensor is inactive.
+int setResetTime = 100; // Reset sensor after setResetTime.
+int setHandToucheValue = 500; // Sensor value when hand touched.
+int countHandTouche = 0; // Counts the time where hand touches.
+int setHandToucheTime = 50; // Set value when trigger when hand touched.
 String ledStatus = "OFF";
+
 
 void setup() {
   Serial.begin(115200);
@@ -33,40 +41,59 @@ void setup() {
 
 void loop() {
   bool buttonState = digitalRead(PIN_TAS);
-  int sensitivityValue = map(analogRead(PIN_REG), 0, 1023, 0, sensitivityMAX);
+  int sensitivityValue = map(analogRead(PIN_REG), 0, 1023, 0, setSensitivityMax);
   long measurement = sensor.capacitiveSensor(30);
 
   Serial.print(ledStatus);
+  Serial.print(countWaitingTime);
+  Serial.print("-");
   Serial.print(sensitivityValue);
   Serial.print("<");
-  Serial.print(delayTime);
+  Serial.print(countDelayTime);
   Serial.print("_");
-  Serial.print(ledOnTime);
+  Serial.print(countLedOnTime);
   Serial.print(">");
   Serial.println(measurement);
 
   if (buttonState || measurement > sensitivityValue || ledActive) {
-    delayTime++;
-    if (delayTime >= setDelayTime || delayActive) {
+    countDelayTime++;
+    if (countDelayTime >= setDelayTime || delayActive) {
       delayActive = true;
       digitalWrite(PIN_LED, HIGH);
       ledActive = true;
       ledStatus = "ON--";
     }
-  } else {
-    resetLED();
-  }
+  } else { resetLED(); }
 
   if (ledActive) {
-    ledOnTime++;
-    delayTime = 0;
-    if (ledOnTime >= setLEDtime) {
-      resetLED();
+    countLedOnTime++;
+    countDelayTime = 0;
+    if (countLedOnTime >= setLEDtime) { resetLED(); }
+    if (buttonState || measurement > sensitivityValue) { countLedOnTime = 0; }
+  }
+
+  if ((measurement < sensitivityValue) && !ledActive) { // Reset sensor after setResetTime.
+    countWaitingTime++;
+    if (countWaitingTime >= setResetTime) {
+      sensor.reset_CS_AutoCal();
+      Serial.println(">>> RESETED <<<");
+      countWaitingTime = 0;
+      //delay(500);
     }
-    if (buttonState || measurement > sensitivityValue) {
-      ledOnTime = 0;
+  } else { countWaitingTime = 0; }
+
+  if (ledActive) { 
+    countLedActiveTime++;
+    if (measurement > setHandToucheValue) {
+      countHandTouche++;
+      Serial.println(countHandTouche);
+      if (countHandTouche >= setHandToucheTime) {
+        sensor.reset_CS_AutoCal();
+        countHandTouche = 0;
+      }
     }
   }
+
   delay(10);
 }
 
@@ -75,6 +102,6 @@ void resetLED() {
   ledActive = false;
   ledStatus = "OFF-";
   delayActive = false;
-  delayTime = 0;
-  ledOnTime = 0;
+  countDelayTime = 0;
+  countLedOnTime = 0;
 }
